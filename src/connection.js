8900edca758c4b3785ced968385292426f92ec83
rekhan <rekhan@expediagroup.com>
2023-02-17 12:05:41 +0000

    creating connection to db

---
+++ src/connection.js
@@ -0,0 +1,14 @@
+const { DataTypes, Sequelize } = require("sequelize");
+const path = require("path");
+
+//Making the connection
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+  define: {
+    timestamps: false,
+  },
+});
+
+module.exports = sequelize;
