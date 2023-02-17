9d1cbda954a8a4be0a4e4092da697ac75b48ac60
rekhan <rekhan@expediagroup.com>
2023-02-17 12:10:08 +0000

    finished writing the models

---
+++ models/Menus.js
@@ -0,0 +1,10 @@
+// The Menu model should have title which is also a string.
+
+const sequelize = require("../src/connection");
+const { DataTypes } = require("sequelize");
+
+const Menus = sequelize.define("Menu", {
+  title: DataTypes.STRING,
+});
+
+module.exports = { Menus };
---
+++ models/Restaurant.js
@@ -0,0 +1,12 @@
+// The Restaurant model should have name, location and cuisine properties, all of which are strings.
+
+const sequelize = require("../src/connection");
+const { DataTypes } = require("sequelize");
+
+const Restaurants = sequelize.define("restaurants", {
+  name: DataTypes.STRING,
+  location: DataTypes.STRING,
+  cuisine: DataTypes.STRING,
+});
+
+module.exports = { Restaurants };
